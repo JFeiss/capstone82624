558624ae5509d355cf70faf76d6d9af1a4deb97e
JFeiss <150168366+JFeiss@users.noreply.github.com>
2024-08-26 18:45:11 -0400

    Update admin.js

--- backend/config/admin.js
+++ backend/config/admin.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
     secret: env('ADMIN_JWT_SECRET'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('7689574edd05d5f7435d2bf6f8438bc08e487b93a8abd4dde7d531b39bf35b9d44adb709dc6f2999bdbfe4702a3e0ded6c0dc1220ade543ce022387404ecc6ee5ec437ae5e3a0b6c12630ec7cbcdfd2c6b2a10d3b733e0d61e67a05dbdbba2746ca5ed9b7c9069cf8b26522e4d95930c7623b0362558ef66d6b26b3412951ea6'),
   },
   transfer: {
     token: {
