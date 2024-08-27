026bd65e59760d95228b0aa30f53dd50dbc7d2e6
JFeiss <150168366+JFeiss@users.noreply.github.com>
2024-08-27 11:47:10 -0400

    Update RestaurantList.jsx

--- frontend/components/RestaurantList.jsx
+++ frontend/components/RestaurantList.jsx
@@ -34,7 +34,7 @@ function RestaurantCard({ data }) {
           height={300}
           width={300}
           srcset={`${process.env.STRAPI_URL || "https://capstone82624.onrender.com"}${
-            data.attributes.image?.data?.attributes?.url ?? ""
+             data.attributes.image.data[0]?.attributes?.url || ""
           }`}
           alt=""
         />
